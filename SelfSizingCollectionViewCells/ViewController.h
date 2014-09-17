f0573ab83e0a865fc83a79b6a5293f26308da4d1
robby <rabaya@gmail.com>
2014-09-17 10:11:01 -0400

    Initial Commit

---
+++ SelfSizingCollectionViewCells/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelfSizingCollectionViewCells
+//
+//  Created by Robby Abaya on 9/17/14.
+//  Copyright (c) 2014 rawbee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
